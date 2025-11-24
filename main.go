bed393b279e3a45af473216f6f01ff95a8e06e33
ety001 <work@akawa.ink>
2025-11-25 01:35:17 +0800

    feat(sdk): add main entry point and client integration
    - Add sdk.go as main entry point exporting Client, API, Broadcast, Auth
    - Update main.go package from main to steemgosdk
    - Update main_test.go package to steemgosdk_test
    - Add GetAPI(), GetBroadcast(), GetAuth() methods to client
    - Integrate API, Broadcast, and Auth layers with client

--- main.go
+++ main.go
@@ -1,13 +1,1 @@
-package main
-
-import (
-	"github.com/steemit/steemgosdk/client"
-)
-
-func GetClient(url string) (c *client.Client) {
-	c = &client.Client{
-		Url:      url,
-		MaxRetry: 5,
-	}
-	return
-}
+package steemgosdk
